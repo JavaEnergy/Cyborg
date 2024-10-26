f22e74af6ab3978a289e671680e00d4b829d562d
Beka Chogovadze <chogovadzebeka@gmail.com>
2024-10-26 19:07:59 +0400

    change home page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-// src/pages/Home.jsx
+// Home.jsx
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './Home.css'; // Import the CSS for styling
@@ -7,21 +7,30 @@ const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="home-container">
-      <div className="content">
-        <h1>{t('home.title')}</h1>
-        {/* <h2>{t('home.subtitle')}</h2>
-
-        <section className="vision-mission">
-          <p>{t('home.vision_content')}</p>
-        </section> */}
-
-        {/* <section className="it-wish">
-          <h2>{t('home.wish_title')}</h2>
-          <p>{t('home.wish_content')}</p>
-        </section> */}
+    <>
+      <div className="hero">
+        <div className="hero-content">
+          <h1>{t('home.title')}</h1>
+        </div>
       </div>
-    </div>
+      <div className="main-content">
+        <div className="image-container">
+          <img src="/src/assets/images/codesurf-caia_image-alamy.jpg" alt="" />
+        </div>
+        <div className="text-container">
+          <h2>{t('home.subtitle')}</h2>
+
+          <section className="vision-section">
+            <p>{t('home.vision_content')}</p>
+          </section>
+
+          <section className="wish-section">
+            <h2>{t('home.wish_title')}</h2>
+            <p>{t('home.wish_content')}</p>
+          </section>
+        </div>
+      </div>
+    </>
   );
 };
 
